65eb60f7a815445cf6ab27641ba29a4ce0793c99
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-11-24 15:39:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const { query } = require('express');
+require('dotenv').config();
+var jwt = require('jsonwebtoken');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+async function run() {
+    try {
+        const productCategoriesCollections = client.db('resaleMarket').collection('categories');
+        const productCollections = client.db('resaleMarket').collection('products');
+        const usersCollections = client.db('resaleMarket').collection('users');
+
+        // All Categories
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = productCategoriesCollections.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        })
+
+
+        // All Products
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productCollections.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        // All Users
+        app.get('/users', async (req, res) => {
+            const query = {}
+            const cursor = usersCollections.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+    }
+    finally {
+
+    }
+}
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7apvnd5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// console.log("URI - ", uri);
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log('MongoDB Database connnectfully.');
+//     client.close();
+// });
+
+
+
+app.get('/', (req, res) => {
+    res.send('Resale Market Server is running ...............')
+})
+
+app.listen(port, () => {
+    console.log('The Reasale Market Server is running on port: ', port);
+})
